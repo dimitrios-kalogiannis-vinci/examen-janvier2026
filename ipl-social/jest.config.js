9a29893241feb8b953780d7f29fd56a0662c6461
dimitrios-kalogiannis-vinci <dimitrios.kalogiannis@student.vinci.be>
2026-01-16 16:27:10 +0100

    commit initial

---
+++ ipl-social/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/tests'],
+    testMatch:['**/*.tests.ts'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70,
+        },
+    },
+    };
